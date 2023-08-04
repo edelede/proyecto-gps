9a0f893b29c693ee503b96f1865eb99637c923ef
JorgeFernandezEnriquez <jorge.fernandez1801@alumnos.ubiobio.cl>
2023-08-03 22:01:02 -0400

    mondongo

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const Prueba = require('../models/modelo');
 
-router.get('/', (req,res) => {
-    res.send('Hello');
+router.get('/', async (req, res) => {
+    try {
+        const modelos = await Prueba.find().exec();
+        console.log(modelos);
+        res.send(modelos); 
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error en el servidor'); 
+    }
 });
+
+router.post('/', async (req, res) => {
+    try {
+        const nuevoModelo = new Prueba(req.body); // Suponiendo que estás pasando los datos en el cuerpo de la solicitud (req.body)
+        const resultado = await nuevoModelo.save();
+        console.log('Nuevo modelo guardado:', resultado);
+        res.status(201).send(resultado); // 201: Created
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error en el servidor');
+    }
+});
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    
+    try {
+        const resultado = await Prueba.findByIdAndDelete(id);
+        
+        if (resultado) {
+            console.log('Modelo eliminado:', resultado);
+            res.send(resultado);
+        } else {
+            res.status(404).send('No se encontró el modelo');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error en el servidor');
+    }
+});
+
 
 module.exports = router;
