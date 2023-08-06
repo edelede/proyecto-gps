8899ebf9844dbc56de0d044a03a117fb8d4b9e27
JorgeFernandezEnriquez <jorge.fernandez1801@alumnos.ubiobio.cl>
2023-08-06 01:53:22 -0400

    webpack

---
+++ src/app/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Hola carlita</h1>
+                <h2>no te acorday na</h2>
+                <h3>cuando te estaba dando duro duro</h3>
+                <h4>y seco</h4>
+            </div>
+        )
+    }
+}
+
+export default App;
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,12 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import { render } from "react-dom";
 
-class App extends Component{
-    render() {
-        return (
-            <h1>Hola carlita</h1>
-        )
-    }
-}
+import App from './App.jsx';
+
 
 render(<App/>, document.getElementById('app'));
